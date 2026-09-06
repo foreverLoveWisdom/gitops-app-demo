f82f835ea060f06a5583272c3ed15e8a2d80f15b
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2026-09-06 15:04:59 +0000

    feat(status): show live pod status/image/restarts

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,6 +2,9 @@ class StatusController < ApplicationController
   def show
     raw = `kubectl get application nginx-demo -n argocd -o json 2>&1`
     @app = JSON.parse(raw)
+
+    pods_raw = `kubectl get pods -n default -l app=gitops-app-demo -o json 2>&1`
+    @pods = JSON.parse(pods_raw)["items"]
   rescue JSON::ParserError
     @error = raw
   end
